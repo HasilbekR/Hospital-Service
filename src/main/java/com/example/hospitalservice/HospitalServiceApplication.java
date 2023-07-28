2e513b866aebd2a953abab2dd528808e7b7cff88
HasilbekR <hreymbaev@gmail.com>
2023-07-28 15:15:39 +0500

    Initial commit

---
+++ src/main/java/com/example/hospitalservice/HospitalServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.hospitalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class HospitalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalServiceApplication.class, args);
+    }
+
+}
