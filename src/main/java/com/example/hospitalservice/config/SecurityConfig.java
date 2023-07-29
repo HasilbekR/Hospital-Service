b46540c2db53e5bc33c6b5163ee59333ced4eb30
nurabdu8000 <nurabdu8000@gmail.com>
2023-07-29 20:58:35 +0500

    added security(jwt tokens) and configs

---
+++ src/main/java/com/example/hospitalservice/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.hospitalservice.config;
+
+import com.example.hospitalservice.filter.JwtTokenFilter;
+import com.example.hospitalservice.service.AuthenticationService;
+import com.example.hospitalservice.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity()
+public class SecurityConfig {
+
+    private final AuthenticationService authenticationService;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+//                .authorizeHttpRequests((authorizer) -> {
+//                    authorizer
+//
+//                })
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new JwtTokenFilter(authenticationService, jwtService),
+                        UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
