c1a684266a40255c68179c0620830381988b2684
nurabdu8000 <nurabdu8000@gmail.com>
2023-08-09 09:27:05 +0500

    some updates

--- src/main/java/com/example/hospitalservice/dto/LocationRequestDto.java
+++ src/main/java/com/example/hospitalservice/dto/LocationRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.hospitalservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class LocationRequestDto {
+    @NotBlank(message = "Line must not be blank!")
     private Double latitude;
+    @NotBlank(message = "Line must not be blank!")
     private Double longitude;
 }
