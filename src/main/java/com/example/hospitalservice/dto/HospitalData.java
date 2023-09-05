0ad030ba7ae7e8439ba1c7eea7752445692a267f
HasilbekR <hreymbaev@gmail.com>
2023-09-05 16:36:03 +0500

    search hospital by name method added

--- src/main/java/com/example/hospitalservice/dto/HospitalData.java
+++ src/main/java/com/example/hospitalservice/dto/HospitalData.java
@@ -1,13 +1,16 @@
 package com.example.hospitalservice.dto;
 
 import com.example.hospitalservice.Entity.HospitalEntity;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class HospitalData {
     private List<HospitalInfo> hospitals;
     private List<String> cities;
+    private Integer pagesCount;
 }
